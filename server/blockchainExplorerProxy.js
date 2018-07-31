081e570a3e5ba53bc2bf95063cddadd7cdb83077
Yehor Popovych <ypopovych@crossroadlabs.xyz>
2018-07-31 16:42:42 +0300

    Error handling into explorer proxy

--- server/blockchainExplorerProxy.js
+++ server/blockchainExplorerProxy.js
@@ -1,57 +1,83 @@
 require('isomorphic-fetch')
 
-module.exports = function (app, env) {
+class RequestError extends Error {
+  constructor(status, body) {
+    super(body.toString())
+    this.status = status
+  }
+}
+
+module.exports = function(app, env) {
   app.get('/api/addresses/summary/:address', async (req, res) => {
     const address = req.params.address
 
-    const response = await request(
-      `${process.env.CARDANOLITE_BLOCKCHAIN_EXPLORER_PROXY_TARGET}/api/addresses/summary/${address}`
-    )
+    try {
+      const response = await request(
+        `${
+          process.env.CARDANOLITE_BLOCKCHAIN_EXPLORER_PROXY_TARGET
+        }/api/addresses/summary/${address}`
+      )
 
-    return res.status(200).send(response)
+      return res.status(200).send(response)
+    } catch (err) {
+      return res.status(err.status || 500).send({Left: err.message})
+    }
   })
 
   app.get('/api/txs/summary/:txId', async (req, res) => {
     const txId = req.params.txId
 
-    const response = await request(
-      `${process.env.CARDANOLITE_BLOCKCHAIN_EXPLORER_PROXY_TARGET}/api/txs/summary/${txId}`
-    )
+    try {
+      const response = await request(
+        `${process.env.CARDANOLITE_BLOCKCHAIN_EXPLORER_PROXY_TARGET}/api/txs/summary/${txId}`
+      )
 
-    return res.status(200).send(response)
+      return res.status(200).send(response)
+    } catch (err) {
+      return res.status(err.status || 500).send({Left: err.message})
+    }
   })
 
   app.get('/api/txs/raw/:txId', async (req, res) => {
     const txId = req.params.txId
 
-    const response = await request(
-      `${process.env.CARDANOLITE_BLOCKCHAIN_EXPLORER_PROXY_TARGET}/api/txs/raw/${txId}`
-    )
+    try {
+      const response = await request(
+        `${process.env.CARDANOLITE_BLOCKCHAIN_EXPLORER_PROXY_TARGET}/api/txs/raw/${txId}`
+      )
 
-    return res.status(200).send(response)
+      return res.status(200).send(response)
+    } catch (err) {
+      return res.status(err.status || 500).send({Left: err.message})
+    }
   })
 
   app.post('/api/bulk/addresses/utxo', async (req, res) => {
     const addresses = req.body
-    const result = {
-      Right: [],
+
+    try {
+      const result = {
+        Right: [],
+      }
+
+      for (let i = 0; i < addresses.length; i += 10) {
+        const response = await request(
+          `${process.env.CARDANOLITE_BLOCKCHAIN_EXPLORER_PROXY_TARGET}/api/bulk/addresses/utxo`,
+          'POST',
+          JSON.stringify(addresses.slice(i, i + 10)),
+          {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+          }
+        )
+
+        result.Right = result.Right.concat(response.Right)
+      }
+
+      return res.status(200).send(result)
+    } catch (err) {
+      return res.status(err.status || 500).send({Left: err.message})
     }
-
-    for (let i = 0; i < addresses.length; i += 10) {
-      const response = await request(
-        `${process.env.CARDANOLITE_BLOCKCHAIN_EXPLORER_PROXY_TARGET}/api/bulk/addresses/utxo`,
-        'POST',
-        JSON.stringify(addresses.slice(i, i + 10)),
-        {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-        }
-      )
-
-      result.Right = result.Right.concat(response.Right)
-    }
-
-    return res.status(200).send(result)
   })
 }
 
@@ -62,7 +88,8 @@ async function request(url, method = 'get', body = null, headers = {}) {
     body,
   })
   if (res.status >= 400) {
-    throw new Error(res.status)
+    const errorText = await res.text()
+    throw new RequestError(res.status, errorText !== '' ? errorText : 'Unknown error')
   }
 
   return res.json()
